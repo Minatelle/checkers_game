33c4ac96579aef39c1228414d9f0704901972b5f
Minatelle <gminatelle@gmail.com>
2023-02-17 21:10:34 -0300

    ✨Board component

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-board',
-  template: `
-    <p>
-      board works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './board.component.html',
+  styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+
+  board: number[][] = [
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1],
+    [-1, 0, -1, 0, -1, 0, -1, 0],
+    [0, -1, 0, -1, 0, -1, 0, -1]
+  ];
 
 }
